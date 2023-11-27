fbcc52c91b831083342b7902395c8419165afcde
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-11-27 18:48:13 +0600

    adding Event Handler with parameter

--- Simple React/react-concept-06/src/components/Devices/Device/Device.jsx
+++ Simple React/react-concept-06/src/components/Devices/Device/Device.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 const Device = (props) => {
     // eslint-disable-next-line react/prop-types
     const { brand, product, price } = props.device;
-
-    
-
-
+    const addToCart = (price) => {
+        console.log ("Price of the product is : ", price);
+    }
+    const addToCartWithParameter = () => addToCart (price);
     return (
         <div>
             <h3>Device</h3>
@@ -15,6 +15,7 @@ const Device = (props) => {
             <p>Product: {product}</p>
             <p>Price: {price}</p>
             <p>Address: {}</p>
+            <button onClick={addToCartWithParameter}>Add to Cart</button>
             <hr />
         </div>
     );
