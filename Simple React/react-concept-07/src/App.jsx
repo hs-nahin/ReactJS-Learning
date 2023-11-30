aa74af906c33d606b6eea89458211d40b71ef522
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-11-30 15:53:39 +0600

    add to cart

--- Simple React/react-concept-07/src/App.jsx
+++ Simple React/react-concept-07/src/App.jsx
@@ -1,18 +1,18 @@
-import {  } from 'react'
+// App.jsx
+import React from 'react'; // Import the React module from 'react'
 
-import './App.css'
-import Header from './components/Header/Header';
-import Shop from './components/Shop/Shop';
+import './App.css'; // Import the styles from the 'App.css' file
+import Header from './components/Header/Header'; // Import the Header component from the specified path
+import Shop from './components/Shop/Shop'; // Import the Shop component from the specified path
 
-function App() {
-
+function App() { // Define the functional component named App
 
   return (
     <>
-    <Header></Header>
-    <Shop></Shop>
+      <Header></Header> {/* Render the Header component */}
+      <Shop></Shop> {/* Render the Shop component */}
     </>
   )
 }
 
-export default App
+export default App // Export the App component as the default export
--- Simple React/react-concept-07/src/components/Shop/Shop.jsx
+++ Simple React/react-concept-07/src/components/Shop/Shop.jsx
@@ -1,33 +1,39 @@
-import { useEffect, useState } from 'react';
-import './Shop.css';
-import Product from '../Product/Product';
+// Shop.jsx
+import { useEffect, useState } from 'react'; // Import useEffect and useState from 'react'
+import './Shop.css'; // Import the styles from 'Shop.css'
+import Product from '../Product/Product'; // Import the Product component from the specified path
 
-const Shop = () => {
-    const [products, setProducts] = useState([]);
-    useEffect(() => {
-        fetch ('products.json')
-        .then (res => res.json())
-        .then (data => setProducts(data))
-    },[]);
-    const handleAddToCart = (product) => {
-        console.log (product);
+const Shop = () => { // Define the functional component named Shop
+    const [products, setProducts] = useState([]); // Declare state variable 'products' and its setter function 'setProducts'
+    const [cart, setCart] = useState([]); // Declare state variable 'cart' and its setter function 'setCart'
+    useEffect(() => { // Use the useEffect hook to perform side effects in function components
+        fetch ('products.json') // Fetch data from 'products.json'
+        .then (res => res.json()) // Parse the response as JSON
+        .then (data => setProducts(data)) // Set the 'products' state with the fetched data
+    },[]); // Empty dependency array to run the effect only once
+
+    const handleAddToCart = (product) => { // Define a function 'handleAddToCart' that adds a product to the cart
+        const newCart = [...cart, product]; // Create a new array with the existing cart items and the new product
+        setCart(newCart); // Update the 'cart' state with the new cart array
     }
+
     return (
         <div className='shop-container'>
             <div className="products-container">
                 {
                     products.map(product => <Product
-                    key={product.id}
-                    product = {product}
-                    AddToCart = {handleAddToCart}
-                    ></Product>)
+                        key={product.id}
+                        product={product}
+                        AddToCart={handleAddToCart}
+                    ></Product>) // Map over the products and render a Product component for each
                 }
             </div>
             <div className="cart-container">
                 <h4>Orders</h4>
+                <p>Selected Items: {cart.length}</p> {/* Display the number of items in the cart */}
             </div>
         </div>
     );
 };
 
-export default Shop;
+export default Shop; // Export the Shop component as the default export
