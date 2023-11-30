d0342af3fcb028b981ead979871af981cd670987
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-11-30 15:12:47 +0600

    adding product cards : DONE

---
+++ Simple React/react-concept-07/src/components/Product/Product.jsx
@@ -0,0 +1,20 @@
+import { } from 'react';
+import './Product.css';
+const Product = (props) => {
+    // eslint-disable-next-line react/prop-types
+    const { img, name, seller, quantity, price, ratings } = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className="card">
+                <h5 className='product-name'>{name}</h5>
+                <h6 className='product-price'>Price : ${price}</h6>
+                <p className='made-by'>Manufacturer : {seller}</p>
+                <p className='rating'>Rating : {ratings}</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
