34ee5ea53ac4b7eb43e72e2d5b6fde25bf726749
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-01-04 16:56:11 +0600

    dynamic data loader and navigate on button click

--- React Router And States/RRS-P2/src/component/Posts/PostDetail/PostDetails.jsx
+++ React Router And States/RRS-P2/src/component/Posts/PostDetail/PostDetails.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import './PostDetails.css';
 const PostDetails = () => {
     const post = useLoaderData();
     const {id, body} = post;
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate(-1);
+    }
     return (
         <div className='post-detail'>
-            <h2>Post No : {id}</h2>
-            <h3>Post : {body}</h3>
+            <h2>Post No : <span>{id}</span></h2>
+            <h3><span>Post :</span> {body}</h3>
+            <button onClick={handleGoBack}>Go Back</button>
         </div>
     );
 };
