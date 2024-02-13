5930e183bd6137a7488855a74fdb9bb744f70247
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-02-14 02:50:05 +0600

    react-router-p3

---
+++ React Router And States/RRS-P3/src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <>
+        <div>
+            <h1>Order Review</h1>
+        </div>
+        </>
+    );
+};
+
+export default OrderReview;
---
+++ React Router And States/RRS-P3/src/main.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Main from './components/Layout/Main';
+import OrderReview from './components/OrderReview/OrderReview';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Main/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: 'review',
+        element: <OrderReview/>,
+      },
+      {
+        path: 'about',
+        element: <About/>,
+      },
+      {
+        path: 'contact',
+        element: <Contact/>,
+      },
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
