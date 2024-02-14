e3a8faec3ac3c1a2305bd73289520e3d2bdb57f9
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-02-14 19:44:30 +0600

    using ternary operator

--- React Router And States/RRS-P3/src/components/Cart/Cart.jsx
+++ React Router And States/RRS-P3/src/components/Cart/Cart.jsx
@@ -6,10 +6,15 @@ const Cart = ({cart, handleRemoveFromCart}) => {
     let message;
     if (cart.length === 0){
         message = <p>Please add product</p>
+    } else {
+        message = <div>
+            <p>Item Added</p>
+        </div>
     }
     return (
         <div>
-            <h1>Order Item List: {cart.length}</h1>
+            <h1>Product List</h1>
+            {cart.length > 0 ? <h4>Total Product: {cart.length}</h4> : <span>The Cart is empty</span>}
             {message}
             {
                 cart.map(tshirt => <p 
@@ -25,5 +30,7 @@ const Cart = ({cart, handleRemoveFromCart}) => {
 
 /*
 * Conditional Rendering
+* 1. Use if-else to set a variable that will contain an element, components.
+* 2. Ternary : Condition ? "if true" : "false"
 */
 export default Cart;
