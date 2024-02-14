75d239a367bc799e725caf941f21a6b0c9da8837
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-02-14 20:10:55 +0600

    Conditional Rendering And Conditional Css Class

--- React Router And States/RRS-P3/src/components/Cart/Cart.jsx
+++ React Router And States/RRS-P3/src/components/Cart/Cart.jsx
@@ -13,8 +13,8 @@ const Cart = ({cart, handleRemoveFromCart}) => {
     }
     return (
         <div>
-            <h1>Product List</h1>
-            {cart.length > 0 ? <h4>Total Product: {cart.length}</h4> : <span>The Cart is empty</span>}
+            <h1 className='product-list'>Product List</h1>
+            {cart.length > 0 ? <h4 className={cart.length ===1 ? 'tomato' : 'blue'}>Total Product: {cart.length}</h4> : <span>The Cart is empty</span>}
             {message}
             {
                 cart.map(tshirt => <p 
@@ -23,6 +23,12 @@ const Cart = ({cart, handleRemoveFromCart}) => {
                         <button onClick={() => handleRemoveFromCart(tshirt._id)}>⛌</button>
                         </p>)
             }
+            {
+                cart.length === 4 && <p>You got a free delivery service for more than four product</p>
+            }{
+                cart.length === 3 || <p>It is less than 3</p>
+            }
+            <p className={`text-color ${cart.length === 3 ? 'tomato' : 'blue'}`}>Conditional CSS Test</p>
         </div>
     );
 };
@@ -30,7 +36,16 @@ const Cart = ({cart, handleRemoveFromCart}) => {
 
 /*
 * Conditional Rendering
-* 1. Use if-else to set a variable that will contain an element, components.
-* 2. Ternary : Condition ? "if true" : "false"
+* 1. Use if or if-else to set a variable that will contain an element, components.
+* 2. Ternary : Condition ? "if true" : "false".
+* 3. Logical &&: (if the condition is true then the next thing will be displayed). 
+* 4. Logical || (if the condition is false then the next thing will be displayed). 
+* 5. Conditional CSS Class
+*/
+
+/*
+* Conditional CSS Class
+* 1. use Ternary
+* 2. Ternary inside template String
 */
 export default Cart;
