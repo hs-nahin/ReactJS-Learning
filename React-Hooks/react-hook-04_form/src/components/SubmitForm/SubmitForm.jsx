1ee3b278aa22367e213cd4af60998c0289a2956f
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-07-03 22:10:31 +0600

    conditional rendering

--- React-Hooks/react-hook-04_form/src/components/SubmitForm/SubmitForm.jsx
+++ React-Hooks/react-hook-04_form/src/components/SubmitForm/SubmitForm.jsx
@@ -1,28 +1,37 @@
 import React, { useState } from "react";
 
 const SubmitForm = () => {
-    const [text, setText] = useState('');
-    const [submit, setSubmit] = useState(false);
+  const [text, setText] = useState("Hello");
+  const [submit, setSubmit] = useState(false);
 
-    const submitForm = event => {
-        event.preventDefault();
-        setSubmit(true);
-    }
+  const submitForm = (event) => {
+    event.preventDefault();
+    setSubmit(true);
+  };
 
-    const nameText = (e) => {
-        const value = e.target.value;
-        setText(value);
-    }
+  const nameText = (e) => {
+    const value = e.target.value;
+    setText(value);
+  };
   return (
     <div>
       <form onSubmit={submitForm}>
-        {submit ? `Hello ${text}` : 'Hello'}
+        {submit ? `Hello ${text}` : "Hello"}
         <br />
-        <input onChange={nameText} type="text" placeholder="Enter Your Name" name="text" />
+        <input
+          onChange={nameText}
+          type="text"
+          placeholder="Enter Your Name"
+          name="text"
+        />
         <br />
-        <input type="email" placeholder="Enter Your Email" name="email"/>
+        <input type="email" placeholder="Enter Your Email" name="email" />
         <br />
-        <input type="password" placeholder="Enter Your Password" name="password"/>
+        <input
+          type="password"
+          placeholder="Enter Your Password"
+          name="password"
+        />
         <br />
         <input type="submit" value="Sign Up" />
       </form>
