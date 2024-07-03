37d716e927a26437d588edd6fd0db309b7a10761
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2024-07-03 22:01:26 +0600

    react form

---
+++ React-Hooks/react-hook-04_form/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import SubmitForm from './components/SubmitForm/SubmitForm'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Submit Form</h1>
+      <SubmitForm/>
+    </div>
+  )
+}
+
+export default App
---
+++ React-Hooks/react-hook-04_form/src/components/SubmitForm/SubmitForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const SubmitForm = () => {
+    const [text, setText] = useState('');
+    const [submit, setSubmit] = useState(false);
+
+    const submitForm = event => {
+        event.preventDefault();
+        setSubmit(true);
+    }
+
+    const nameText = (e) => {
+        const value = e.target.value;
+        setText(value);
+    }
+  return (
+    <div>
+      <form onSubmit={submitForm}>
+        {submit ? `Hello ${text}` : 'Hello'}
+        <br />
+        <input onChange={nameText} type="text" placeholder="Enter Your Name" name="text" />
+        <br />
+        <input type="email" placeholder="Enter Your Email" name="email"/>
+        <br />
+        <input type="password" placeholder="Enter Your Password" name="password"/>
+        <br />
+        <input type="submit" value="Sign Up" />
+      </form>
+    </div>
+  );
+};
+
+export default SubmitForm;
